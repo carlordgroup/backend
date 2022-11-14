03a80f52d7072890156dd7f94a0f9eb37fac4c54
lynkas <cat@catting.net>
2022-11-14 12:17:01 -0500

    CAR-17: Debug database

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"carlord/ent"
+	"context"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	r := gin.Default()
+	db, _ := os.LookupEnv("DATABASE")
+	client, err := ent.Open("postgres", db)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	count, err := client.User.Query().Count(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	print(count)
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
