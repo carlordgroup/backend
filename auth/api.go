b6c08ba5cbee6a46fa14190f2f24c5d964e7e7ad
lynkas <cat@catting.net>
2022-11-14 22:52:01 -0500

    CAR-21: Backend readme and API documentation

--- auth/api.go
+++ auth/api.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"carlord/ent"
 	"encoding/base64"
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
@@ -12,18 +13,39 @@ type service struct {
 	client *ent.Client
 }
 
-func New(client *ent.Client, group *gin.Engine) {
+func New(client *ent.Client, group gin.IRoutes) *jwt.GinJWTMiddleware {
 	s := service{
 		client: client,
 	}
 	auth := s.initMiddleware()
-	group.POST("/login", auth.LoginHandler)
+	group.POST("/login", login(auth))
 	group.POST("/refresh", auth.RefreshHandler)
 	group.POST("/register", s.register)
 	group.GET("/", auth.MiddlewareFunc(), s.self)
-
+	return auth
 }
 
+type token struct{ Token string }
+
+// login godoc
+// @Summary login
+// @Description login an account
+// @Param request body LoginCredential true "email password"
+// @Accept json
+// @Produce json
+// @Success 200 {object} token
+// @Router /account/login [get]
+func login(auth *jwt.GinJWTMiddleware) gin.HandlerFunc {
+	return auth.LoginHandler
+}
+
+// Self godoc
+// @Summary get login user info
+// @Description will respond user account info when user login
+// @Accept json
+// @Produce json
+// @Success 200 {object} ent.Account
+// @Router /account/ [get]
 func (s *service) self(ctx *gin.Context) {
 	u, err := s.client.Account.Get(ctx, ctx.MustGet("id").(int))
 	if err != nil {
@@ -34,6 +56,13 @@ func (s *service) self(ctx *gin.Context) {
 
 }
 
+// Register godoc
+// @Param request body LoginCredential true "email password"
+// @Summary register a user
+// @Accept json
+// @Produce json
+// @Success 200 {object} ent.Account
+// @Router /account/register [post]
 func (s *service) register(ctx *gin.Context) {
 	var login LoginCredential
 	err := ctx.ShouldBindJSON(&login)
